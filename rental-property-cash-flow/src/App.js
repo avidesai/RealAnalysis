f3a24a4530dc22e590dd792d0be71140da45a5e4
avi <avidesai2@gmail.com>
2024-06-17 22:09:43 -0700

    First commit

---
+++ rental-property-cash-flow/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CashFlowAnalysis from './CashFlowAnalysis';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CashFlowAnalysis />
+    </div>
+  );
+}
+
+export default App;
